18e2a67cead9f82bff8e92dd1979358b03381f7f
thomascchu <thomas.chu@yale.edu>
2015-11-16 10:13:07 -0500

    Working Regions using the distance formula!!!!

--- West Campus/ScavengerHuntViewController.swift
+++ West Campus/ScavengerHuntViewController.swift
@@ -14,7 +14,7 @@ class ScavengerHuntViewController: MyViewController, CLLocationManagerDelegate {
     
     var locationManager: CLLocationManager!
     let CEIDLoc = CLLocationCoordinate2D(latitude: 41.312788, longitude: -72.925319)
-    let regionRadius : Double! = 30.0
+    let regionRadius : Double! = 10.0
     
     @IBOutlet weak var RegionMonitor: UILabel!
     @IBAction func buttonPressed(sender: AnyObject) {
@@ -41,13 +41,18 @@ class ScavengerHuntViewController: MyViewController, CLLocationManagerDelegate {
     
     func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]){
         
-        let destLat = (M_PI/180)*(90-CEIDLoc.latitude)
-        let destLong = (M_PI/180)*CEIDLoc.longitude
+        let destLat = (CEIDLoc.latitude)
+        let destLong = CEIDLoc.longitude
         
-        let curLat = (M_PI/180)*(90-locations[0].coordinate.latitude)
-        let curLong = (M_PI/180)*locations[0].coordinate.longitude
+        let curLat = (locations[0].coordinate.latitude)
+        let curLong = locations[0].coordinate.longitude
         
-        let distance = 6371000 * acos(sin(destLat) * sin(curLat)*cos(curLong-destLong) + cos(destLong)*cos(curLong))   //calculate distance from starting point using the spherical coordinate formula D = Radius * arccos(sin φ1 sin φ2 cos(θ1-θ2) + cos φ1 cos φ2)
+        let x = 111111*(curLat-destLat)
+        
+        let y = 111111*(curLong-destLong)
+        
+        let distance :Double = sqrt(x*x+y*y)
+        
         distanceLabel.text = String(distance)
         
         if (distance < regionRadius){
