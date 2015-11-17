7d484aba6f857f2bed7383a662b8ee4492d26d61
jshan0922 <danyang.shan@yale.edu>
2015-11-17 10:59:44 -0500

    Map Zoom feature for Demo Project

--- West Campus/MapViewController.swift
+++ West Campus/MapViewController.swift
@@ -1,3 +1,12 @@
+
+
+
+
+
+
+
+
+
 //
 //  mapView.swift
 //  West Campus
@@ -15,15 +24,25 @@ class MapViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         map.mapType = MKMapType.Satellite
-        map.zoomEnabled = false
+        //map.zoomEnabled = false
         map.scrollEnabled = false
-        map.setCenterCoordinate(CLLocationCoordinate2D(latitude: 41.3125884, longitude: -72.9249614), animated: true)
+        let initialLocation = CLLocationCoordinate2D(latitude: 41.3125884, longitude: -72.9249614)
+        map.setCenterCoordinate(initialLocation, animated: true)
+        
+        let regionRadius: CLLocationDistance = 1000
+        func centerMapOnLocation(location: CLLocationCoordinate2D) {
+            let coordinateRegion = MKCoordinateRegionMakeWithDistance(location,
+                regionRadius * 2.0, regionRadius * 2.0)
+            map.setRegion(coordinateRegion, animated: true)
+        }
+
         //This needs to be configured so that the rectangle covers the appropriate part of the map
         //map.setVisibleMapRect(mapRect: MKMapRectanimated true)
+        centerMapOnLocation(initialLocation)
     }
     
-    override func didReceiveMemoryWarning() {
+    /*override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
+    }*/
 }
