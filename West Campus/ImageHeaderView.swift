8e56107502004cf080508a124a592422d8063536
jared weinstein <jaredweinstein1@gmail.com>
2015-11-22 20:01:22 -0500

    see last commit

--- West Campus/ImageHeaderView.swift
+++ West Campus/ImageHeaderView.swift
@@ -6,8 +6,13 @@
 //  Copyright © 2015 ENAS118. All rights reserved.
 //
 
-import Cocoa
+//This class displays a correct header image for all the application and the header for the main page.
+//Hopefully this will pull from the database image
+//If internet connetion is unavalible this class will use a default west campus image that we have saved
+
+import Foundation
+import UIKit
 
 class ImageHeaderView: UIView {
-
+    
 }
