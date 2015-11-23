70f189ce126291dd829d6a34df27355dfb8c165a
jared weinstein <jaredweinstein1@gmail.com>
2015-11-22 20:00:59 -0500

    Nothing noteworthy... added a new image view class that will hopefully display content downloaded form the database eventually.

---
+++ West Campus/ImageHeaderView.swift
@@ -0,0 +1,13 @@
+//
+//  ImageHeaderView.swift
+//  West Campus
+//
+//  Created by jared weinstein on 11/22/15.
+//  Copyright © 2015 ENAS118. All rights reserved.
+//
+
+import Cocoa
+
+class ImageHeaderView: UIView {
+
+}
