e5dcfe9f2efc46c2750a1c307592a77cbae6e395
jared weinstein <jaredweinstein1@gmail.com>
2015-11-26 23:52:29 -0700

    Started UI standardization by providing a new background

--- West Campus/MyViewController.swift
+++ West Campus/MyViewController.swift
@@ -11,4 +11,21 @@ import UIKit
 
 class MyViewController : UIViewController{
     static var model : MainModel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //This loads a pretty background and adds it to all views
+        let imageView = UIImageView(frame: self.view.frame); // set as you want
+        let image = UIImage(named: "background");
+        imageView.image = image;
+        self.view.addSubview(imageView);
+        self.view.sendSubviewToBack(imageView)
+
+    }
+    
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        //whether or not the top status bar is white or black
+        return UIStatusBarStyle.LightContent
+    }
 }
