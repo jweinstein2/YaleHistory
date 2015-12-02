ff2f8ebfcabc5c9a7bd9831193b6d1b9f1baf4ab
jshan0922 <danyang.shan@yale.edu>
2015-12-02 10:03:00 -0500

    banner for about app

---
+++ West Campus/AboutThisAppViewController.swift
@@ -0,0 +1,41 @@
+//
+//  AboutThisAppViewController.swift
+//  West Campus
+//
+//  Created by Julia Shan on 12/2/15.
+//  Copyright © 2015 ENAS118. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class AboutThisAppViewController: MyViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    
+    }
+    @IBAction func buttonPressed(sender: AnyObject) {
+        self.dismissViewControllerAnimated(false, completion: nil);
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
