3f677ce4e6df59936aa033f2c34645b342b65c37
thomascchu <thomas.chu@yale.edu>
2015-12-09 18:23:49 -0500

    Added Contributors to parser

--- West Campus/Project.swift
+++ West Campus/Project.swift
@@ -15,15 +15,16 @@ class Project {
     var gpsLatitude: Double
     var gpsLongitude: Double
     var clue: String
-    var action: String?
-    var imageLink : String?
+    var action: String
+    var contributors: String
+    var imageLink : String
     var innovations: Bool
     var ecology: Bool
     var health: Bool
     var radius: Int
 
     
-    init(projectId: String, title: String, summary: String, link: String, gpsLatitude: Double, gpsLongitude: Double, clue: String, action: String?, imageLink: String?, innovations: Bool, ecology: Bool, health: Bool, radius: Int){
+    init(projectId: String, title: String, summary: String, link: String, gpsLatitude: Double, gpsLongitude: Double, clue: String, action: String, contributors: String, imageLink: String, innovations: Bool, ecology: Bool, health: Bool, radius: Int){
         self.projectId = projectId
         self.title = title
         self.summary = summary
@@ -32,8 +33,8 @@ class Project {
         self.gpsLongitude = gpsLongitude
         self.clue = clue
         self.action = action
-        self.imageLink = imageLink //"http://www.planwallpaper.com/static/images/Winter-Tiger-Wild-Cat-Images.jpg"
-        
+        self.imageLink = imageLink
+        self.contributors = contributors
         
         self.innovations = innovations
         self.ecology = ecology
